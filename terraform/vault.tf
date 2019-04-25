63044fd937a820e658fc4aec1f50b11dbaaed162
Tom Ford <tom.ford@asu.edu>
2019-04-25 11:40:03 -0700

    added write password to vault

---
+++ terraform/vault.tf
@@ -0,0 +1,29 @@
+variable "vault_addr" {
+  description = "URL of Vault Server"
+}
+
+variable "vault_token" {
+  description = "Vault Token"
+}
+
+provider "vault" {
+  address = "${var.vault_addr}"
+  token   = "${var.vault_token}"
+}
+
+variable "vault_role_ttl" {
+  default     = "3600"
+  description = "The TTL of the Vault-created db credentials"
+}
+
+resource "vault_generic_secret" "rds_oracle_master" {
+  path = "secret/database/asupmcld/asupmcld-np"
+
+  data_json = <<EOT
+{
+  "username":   "${var.pm_username}",
+  "password":   "${random_string.pm_master_password.result}",
+  "endpoint":   "${aws_db_instance.pm_ldw_oracle.endpoint}"
+}
+EOT
+}
