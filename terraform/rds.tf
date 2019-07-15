a2b064ad496d2ec45d022643bddff855ed9dba6b
Tom Ford <tom.ford@asu.edu>
2019-07-15 11:29:45 -0700

    change SHA1 to SHA256

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -73,7 +73,7 @@ resource "aws_db_option_group" "pm_ldw_oracle" {
 
     option_settings {
       name  = "SQLNET.CRYPTO_CHECKSUM_TYPES_SERVER"
-      value = "SHA1"
+      value = "SHA256"
     }
   }
 
