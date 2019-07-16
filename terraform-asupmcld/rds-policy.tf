e24b43008860e4305e5f708adac26cbf2bbe0795
Tom Ford <tom.ford@asu.edu>
2019-07-16 11:20:22 -0700

    fixed reference to RDSReadOnlyWithSnapshots

--- terraform-asupmcld/rds-policy.tf
+++ terraform-asupmcld/rds-policy.tf
@@ -25,12 +25,12 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "rds_allow_snapshots_policy_attach" {
-  role       = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/RDSReadOnlyWithSnapshots"
+  role       = "RDSReadOnlyWithSnapshots"
   policy_arn = "${aws_iam_policy.allow_snapshots_role_policy.arn}"
 }
 
 resource "aws_iam_role_policy_attachment" "rds_dba_policy_attach" {
-  role       = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/RDSReadOnlyWithSnapshots"
+  role       = "RDSReadOnlyWithSnapshots"
   policy_arn = "arn:aws:iam::aws:policy/AmazonRDSReadOnlyAccess"
 }
 
@@ -56,6 +56,6 @@ resource "aws_iam_policy" "ldw_allow_pi" {
 }
 
 resource "aws_iam_role_policy_attachment" "ldw_allow_pi_policy_attach" {
-  role       = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/RDSReadOnlyWithSnapshots"
+  role       = "RDSReadOnlyWithSnapshots"
   policy_arn = "${aws_iam_policy.ldw_allow_pi.arn}"
 }
