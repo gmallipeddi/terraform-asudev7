15ad1ba590d7a32a5db9051d58c7c8cec1751cf2
Tom Ford <tom.ford@asu.edu>
2019-07-29 16:13:11 -0700

    added iam users ssbhati3, mwsmit19, schinthi, avichenk

--- terraform-acct/iam-user.tf
+++ terraform-acct/iam-user.tf
@@ -12,6 +12,34 @@ module "user-mrball2" {
   cli-s3-access-arn = "${aws_iam_policy.dba_cli_s3_access.arn}"
 }
 
+module "user-ssbhati3" {
+  source = "./iam-users"
+  iam-user = "ssbhati3"
+  current-account-id = "${data.aws_caller_identity.current.account_id}"
+  cli-s3-access-arn = "${aws_iam_policy.dba_cli_s3_access.arn}"
+}
+
+module "user-mwsmit19" {
+  source = "./iam-users"
+  iam-user = "mwsmit19"
+  current-account-id = "${data.aws_caller_identity.current.account_id}"
+  cli-s3-access-arn = "${aws_iam_policy.dba_cli_s3_access.arn}"
+}
+
+module "user-schinthi" {
+  source = "./iam-users"
+  iam-user = "schinthi"
+  current-account-id = "${data.aws_caller_identity.current.account_id}"
+  cli-s3-access-arn = "${aws_iam_policy.dba_cli_s3_access.arn}"
+}
+
+module "user-avichenk" {
+  source = "./iam-users"
+  iam-user = "avichenk"
+  current-account-id = "${data.aws_caller_identity.current.account_id}"
+  cli-s3-access-arn = "${aws_iam_policy.dba_cli_s3_access.arn}"
+}
+
 resource "aws_iam_policy" "dba_cli_s3_access" {
   name        = "LDWDBAS3access"
   description = "LDW DBA S3 bucket access for Oracle import"
