c122cd1e1ae3ac4850e149e0aace3560d26244f1
Michael North <michael-north@live.com>
2019-09-04 15:12:57 -0700

    update for terraform 0.12

--- terraform-shared/dms-instance.tf
+++ terraform-shared/dms-instance.tf
@@ -1,15 +1,7 @@
 resource "aws_dms_replication_subnet_group" "dms_subnetgroup" {
   replication_subnet_group_description = "dms replication group"
   replication_subnet_group_id          = "pm-dms-1"
-  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-  # force an interpolation expression to be interpreted as a list by wrapping it
-  # in an extra set of list brackets. That form was supported for compatibilty in
-  # v0.11, but is no longer supported in Terraform v0.12.
-  #
-  # If the expression in the following list itself returns a list, remove the
-  # brackets to avoid interpretation as a list of lists. If the expression
-  # returns a single list item then leave it as-is and remove this TODO comment.
-  subnet_ids = [module.vpc.private_ids]
+  subnet_ids                           = module.vpc.private_ids
   tags = merge(
     module.tags.tags,
     {
--- terraform-shared/dms-sg.tf
+++ terraform-shared/dms-sg.tf
@@ -52,17 +52,7 @@ resource "aws_security_group_rule" "dms_oracle_private_out" {
   to_port     = 3200
   protocol    = "tcp"
 
-  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-  # force an interpolation expression to be interpreted as a list by wrapping it
-  # in an extra set of list brackets. That form was supported for compatibilty in
-  # v0.11, but is no longer supported in Terraform v0.12.
-  #
-  # If the expression in the following list itself returns a list, remove the
-  # brackets to avoid interpretation as a list of lists. If the expression
-  # returns a single list item then leave it as-is and remove this TODO comment.
-  cidr_blocks = [
-    module.vpc.private_cidrs,
-  ]
+  cidr_blocks = module.vpc.private_cidrs
 
   security_group_id = aws_security_group.dms-sg.id
 }
