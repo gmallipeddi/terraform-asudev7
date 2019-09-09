eafd0060f924ec5715d232a7d3e6ac52b67646fd
Tom Ford <tom.ford@asu.edu>
2019-09-09 13:03:15 -0700

    removed TODO comments

--- terraform-asupmcld/rds.tf
+++ terraform-asupmcld/rds.tf
@@ -11,14 +11,6 @@ resource "aws_db_instance" "pm_ldw_oracle" {
   password              = random_string.pm_master_password.result
   character_set_name    = var.pm_character_set_name
   license_model         = var.pm_license_model
-  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-  # force an interpolation expression to be interpreted as a list by wrapping it
-  # in an extra set of list brackets. That form was supported for compatibilty in
-  # v0.11, but is no longer supported in Terraform v0.12.
-  #
-  # If the expression in the following list itself returns a list, remove the
-  # brackets to avoid interpretation as a list of lists. If the expression
-  # returns a single list item then leave it as-is and remove this TODO comment.
   vpc_security_group_ids     = [module.rds-sg.id]
   db_subnet_group_name       = aws_db_subnet_group.pm_subnet_group.id
   availability_zone          = var.pm_availability_zone
@@ -53,14 +45,6 @@ resource "aws_db_subnet_group" "pm_subnet_group" {
   name        = "${var.pm_identifier}-subnet-group"
   description = "${var.pm_identifier} RDS Subnet Group"
 
-  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-  # force an interpolation expression to be interpreted as a list by wrapping it
-  # in an extra set of list brackets. That form was supported for compatibilty in
-  # v0.11, but is no longer supported in Terraform v0.12.
-  #
-  # If the expression in the following list itself returns a list, remove the
-  # brackets to avoid interpretation as a list of lists. If the expression
-  # returns a single list item then leave it as-is and remove this TODO comment.
   subnet_ids = module.vpc.private_ids
 }
 
