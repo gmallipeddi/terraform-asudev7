08d9ac7112f6b90c1f27b3de8ddf09ab89b082b7
Tom Ford <tom.ford@asu.edu>
2019-09-12 16:16:29 -0700

    added s3 bucket policy

--- terraform-shared/s3-import.tf
+++ terraform-shared/s3-import.tf
@@ -17,3 +17,32 @@ resource "aws_s3_bucket" "import_bucket" {
   )
 }
 
+data "aws_iam_policy_document" "import_bucket" {
+  statement {
+    sid = "s3integration_bp"
+
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+      "s3:PutObject",
+    ]
+
+    principals {
+      type = "AWS"
+      identifiers = ["arn:aws:iam::306276705765:root"]
+    }
+
+    resources = [
+      "arn:aws:s3:::oracle-data-warehouse-import",
+      "arn:aws:s3:::oracle-data-warehouse-import/*",
+    ]
+  }
+}
+
+resource "aws_s3_bucket_policy" "import_bucket" {
+  bucket = aws_s3_bucket.import_bucket.id
+
+  policy = data.aws_iam_policy_document.import_bucket.json
+}
